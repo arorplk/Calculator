26d6f695038aa5cef3a4859274d39abd38b16c61
Pulkit Arora <arorplk@gmail.com>
2013-02-05 00:32:43 -0500

    Empty Code.

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+README.
+This is a Beginner to Intermediate project.
+This is to create a Calculator using recursion that can perform basic 4 functions plus Square Root and Exponential functions.
+I'd like to get the framework setup and then add a GUI
---
+++ src/calculator/Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Pulkit
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println ("Java Calculator!");
+    
+    }
+}
